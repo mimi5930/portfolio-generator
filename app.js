19e916ece2f01222655d4b9526978f44b5d6a9ae
Mike <mikej.miller440@gmail.com>
2022-01-16 18:15:12 -0600

    edited app

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const [name, github] = profileDataArgs;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
