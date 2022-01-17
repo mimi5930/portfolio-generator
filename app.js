02b91bd60bbbf67b2a5b2948cb9438769d557838
Mike <mikej.miller440@gmail.com>
2022-01-17 10:34:43 -0600

    refactored code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -129,18 +129,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-      return generatePage(portfolioData);
+    return generatePage(portfolioData);
   })
   .then(pageHTML => {
-      return writeFile(pageHTML)
+    return writeFile(pageHTML);
   })
   .then(writeFileResponse => {
-      console.log(writeFileResponse);
-      return copyFile();
+    console.log(writeFileResponse);
+    return copyFile();
   })
   .then(copyFileResponse => {
-      console.log(copyFileResponse);
+    console.log(copyFileResponse);
   })
   .catch(err => {
-      console.log(err);
-  })
+    console.log(err);
+  });
